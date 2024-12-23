f86c7b23fe8ad75bef461e2591d427d922c39645
dat lu <Ludat2410@gmail.com>
2024-12-23 18:05:51 +0700

    UI app

---
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.newsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newsapp.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+       
+    }
+}
