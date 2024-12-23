f36ea36bddf29d3f58b0dc69c9bc6031a12f805b
dat lu <Ludat2410@gmail.com>
2024-12-23 19:02:37 +0700

    retrofit setup

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "21da9fdf0a6f46bea59d2d5ad26c0321"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
